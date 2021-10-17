8c23ad2817878eee786a771b6f0b15520f656a52
D6_49449_GayatriGunjal <gayatrigunjal29@gmail.com>
2021-10-17 14:30:51 +0530

    Empty String Return Zero

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,19 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int calculator(String string) {	
+		 return 0;
+		 
+		 //pass result
+//		//Total tests run: 1, Passes: 1,  Failures: 0, Skips: 0
+//		 
+//		
+//		//fail result
+//		//return -1;
+//		//Tests run: 1, Failures: 1, Skips: 0
+//	
+		 }
+
+
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -2,20 +2,31 @@ package calculator;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
-    
+	//make calculator as field to avoid repeation of code
+	private StringCalculator calculator;
+	
+	
+	//Calculator initiled Here
+	@BeforeTest
+	public void init() {
+		calculator=new StringCalculator();
+	}
+	
+	
+	//1.Empty String Retunn Zero
+	//Result
+	//passed nothing --->return 0
+	@Test
+	public void emptyStringReturnzero() throws Exception {
+	
+		assertEquals(calculator.calculator(""),0);
+	}
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	
+	
 }
